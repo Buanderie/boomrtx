81becc9388c841e115a1ec035da6477a8d4cdd04
Nicolas <cornichon@pop-os.localdomain>
2019-02-18 23:17:21 +0100

    Addition of trigger interface for RX...

---
+++ mcu/rx/trigger.h
@@ -0,0 +1,62 @@
+#ifndef __TRIGGER_H
+#define __TRIGGER_H
+
+class Trigger
+{
+public:
+
+  typedef enum
+  {
+    ACTIVE = 0,
+    INACTIVE
+  } TriggerState;
+
+  Trigger()
+  :_triggerStart(0), _triggerDuration(0), _state(INACTIVE)
+  {
+
+  }
+
+  virtual ~Trigger()
+  {
+
+  }
+
+  void trigger( unsigned long durationMilliseconds )
+  {
+    _triggerStart = millis();
+    _triggerDuration = durationMilliseconds;
+    _state = ACTIVE;
+    onActivation();
+  }
+
+  void tick()
+  {
+    unsigned long dt = (millis() - _triggerStart);
+    if( dt >= _triggerDuration && _state == ACTIVE )
+    {
+      _state = INACTIVE;
+      onDeactivation();
+    }
+  }
+
+  virtual void onActivation()
+  {
+
+  }
+
+  virtual void onDeactivation()
+  {
+
+  }
+
+private:
+  unsigned long _triggerStart;
+  unsigned long _triggerDuration;
+  TriggerState _state;
+
+protected:
+
+};
+
+#endif
